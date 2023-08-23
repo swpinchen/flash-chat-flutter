17a4c47f8e0cdd20311aca91f806c2116509af96
swpinchen <swpinchen@outlook.com>
2023-08-23 20:17:38 +0900

    Example of animated text - flutter package

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'welcome_screen';
@@ -63,13 +64,18 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
                     height: 60.0,
                   ),
                 ),
-                Text(
-                  'Flash Chat',
-                  style: TextStyle(
-                    fontSize: 45.0,
-                    fontWeight: FontWeight.w900,
-                    color: Colors.grey
-                  ),
+                AnimatedTextKit(
+                  animatedTexts: [
+                    TypewriterAnimatedText(
+                      'Flash Chat',
+                      textStyle: TextStyle(
+                        fontSize: 32.0,
+                        fontWeight: FontWeight.w900,
+                        // color: Colors.grey
+                      ),
+                      speed: const Duration(milliseconds: 200),
+                    ),
+                  ]
                 ),
               ],
             ),
